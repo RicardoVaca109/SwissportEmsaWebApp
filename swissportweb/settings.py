ad4f536a5379fe2a5632f054920cd0eacc391a84
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-13 11:02:59 -0500

    Añadir aplicación 'website' a la configuración de INSTALLED_APPS del settings.py

--- swissportweb/settings.py
+++ swissportweb/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+# Definicion de aplciación, añadir las aplicaciones necesarias
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website', # aplicacion website creada 
 ]
 
 MIDDLEWARE = [
