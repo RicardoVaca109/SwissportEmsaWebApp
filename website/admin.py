086c5d716e69046ea5c858662ad9e91036161ba6
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-14 10:38:23 -0500

    Se registra los modelos de las tablas roles y aeropuertos para ver en pestaña admin de DJANGO

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
+# Import neccesary libraries / Importar librerias necesarias
 from django.contrib import admin
 
+# Import the models / Importar los modelos
+from .models import Roles
+from .models import Aeropuerto
+
 # Register your models here.
+# Registrar los modelos aquí
+admin.site.register(Roles) # Table roles / tabla roles
+admin.site.register(Aeropuerto) # Table aeropuertos / tabla aeropuertos
