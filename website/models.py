ab9f325718a041399aedf4aea426bb0338569ca2
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-15 13:05:18 -0500

    Cmabio en el modelo de la tabla aeropuertos, se agrega tipo("Nacional","Internacional")

--- website/models.py
+++ website/models.py
@@ -28,13 +28,25 @@ class Aeropuerto(models.Model): # Table Aeropuertos / Tabla Aeropuertos
     estacion_aeropuerto = models.CharField(max_length = 3, unique = True)
     nombre = models.CharField(max_length = 80, unique = True)
     ciudad = models.CharField(max_length = 80)
-    pais = models.CharField(max_length = 80)
+    pais = models.CharField(max_length = 80)  
+    
+    TIPO_AEROPUERTO_CHOICES = {
+        "Internacional":"Internacional",
+        "Nacional":"Nacional",
+    }
+    
+    tipo_aeropuerto = models.CharField(
+        max_length = 20,
+        choices = TIPO_AEROPUERTO_CHOICES,
+        null = True,
+        blank = True
+    )
     
     def __str__(self):
-        return  f"{self.estacion_aeropuerto} - {self.nombre} ({self.ciudad}, {self.pais})"
+        return  f"{self.estacion_aeropuerto} - {self.nombre} ({self.ciudad}, {self.pais} - Tipo {self.tipo_aeropuerto})"
     
     
-class Usuario(models.Model): # Table Usuarios / Tabla Usuarios
+class Usuario(models.Model): # Table Usuarios / Tabla Usuario
     id_usuario = models.AutoField(primary_key = True)
     id_aeropuerto = models.ForeignKey(Aeropuerto, on_delete = models.CASCADE)
     id_rol = models.ForeignKey(Role, on_delete = models.CASCADE )
