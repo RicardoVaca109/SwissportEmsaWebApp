e050bddc0f889a623a2978fdf8d4d492bcb3a6ef
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-20 12:29:11 -0500

    Urls de modelo aeropuertos y usuarios implementados con sus respectivos templates de HTML.

--- website/urls/dashboard_urls.py
+++ website/urls/dashboard_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from website.views.dashboard_view import navegacion_dashboard_general
 from website.views.aeropuertos_view import navegacion_dashboard_aeropuertos
+from website.views.usuarios_view import navegacion_dashboard_usuarios
 
 urlpatterns = [
     path('', navegacion_dashboard_general, name='dashboard_general'),
-    path('aeropuertos/', navegacion_dashboard_aeropuertos, name='dashboard_aeropuertos'),
+    path('aeropuertos/', navegacion_dashboard_aeropuertos, name = 'dashboard_aeropuertos'),
+    path('usuarios/', navegacion_dashboard_usuarios, name = 'dashboard_usuarios')
 ]
