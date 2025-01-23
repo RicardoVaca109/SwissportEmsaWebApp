c70d9ee1c328ee2ecf4ce9fda630774ae2f15ea4
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-23 08:32:16 -0500

    Se reformula los urls para separar la lógica de Urls por modelo

--- website/urls/dashboard_urls.py
+++ website/urls/dashboard_urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from website.views.dashboard_view import navegacion_dashboard_general
-from website.views.aeropuertos_view import navegacion_dashboard_aeropuertos
-from website.views.usuarios_view import navegacion_dashboard_usuarios
 
 urlpatterns = [
     path('', navegacion_dashboard_general, name='dashboard_general'),
-    path('aeropuertos/', navegacion_dashboard_aeropuertos, name = 'dashboard_aeropuertos'),
-    path('usuarios/', navegacion_dashboard_usuarios, name = 'dashboard_usuarios')
+    path('aeropuertos/', include('website.urls.aeropuertos_urls')),
+    path('usuarios/',include('website.urls.usuarios_urls')),
+    path('vuelos/', include('website.urls.vuelos_urls'))
 ]
