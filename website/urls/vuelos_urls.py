462e83cc43e9a9bc080e96ada9dea51e8cf67112
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-27 08:36:45 -0500

    Rutas Url para añadir y navegar en el modelo Vuelos

--- website/urls/vuelos_urls.py
+++ website/urls/vuelos_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from website.views.vuelos_view import navegacion_dashboard_vuelos
+from website.views.vuelos_view import navegacion_dashboard_vuelos, agregar_add_vuelos
 
 urlpatterns = [
     # Principal Route/ Ruta principal
-    path('', navegacion_dashboard_vuelos, name = 'dahsboard_vuelos')
+    path('', navegacion_dashboard_vuelos, name = 'dashboard_vuelos'),
+    path('agregar/', agregar_add_vuelos, name = 'add_vuelos')
 ]
