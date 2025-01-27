2980c61c15c8bb2f2d61837aab3230798dedeea2
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-27 08:33:15 -0500

    Path Url para agregar datos del modelo Usuarios

--- website/urls/usuarios_urls.py
+++ website/urls/usuarios_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from website.views.usuarios_view import navegacion_dashboard_usuarios
+from website.views.usuarios_view import navegacion_dashboard_usuarios, agregar_add_usuarios
 
 urlpatterns = [
     
-    path('', navegacion_dashboard_usuarios, name ='dashboard_usuarios' )
+    path('', navegacion_dashboard_usuarios, name ='dashboard_usuarios' ),
+    path('agregar/', agregar_add_usuarios, name = 'add_usuarios')
 ]
