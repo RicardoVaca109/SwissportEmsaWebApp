ae72808a024110b07f97c456a722b77efce374f5
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-28 11:06:28 -0500

    Se agregan las nuevas tablas para que el admin de Django pueda administrarlas

--- website/admin.py
+++ website/admin.py
@@ -2,12 +2,15 @@
 from django.contrib import admin
 
 # Import the models / Importar los modelos
-from .models import Role
-from .models import Aeropuerto
-from .models import Usuario
-from .models import Vuelo
-from .models import Evento
-from .models import Atraso
+from website.models import Role
+from website.models import Aeropuerto
+from website.models import Usuario
+from website.models import Vuelo
+from website.models import Evento
+from website.models import Atraso
+from website.models import Acceso
+from website.models import Prueba
+from website.models import Resultado
 
 # Register your models here.
 # Registrar los modelos aquí
@@ -17,3 +20,6 @@ admin.site.register(Usuario)# Table Usuarios / tabla usuarios
 admin.site.register(Vuelo)# Table Vuelos / tabla vuelos
 admin.site.register(Evento) # Table Eventos / tabla eventos
 admin.site.register(Atraso) # Table Atrasos / tabla atrasos
+admin.site.register(Acceso) # Table Acceso / tabla acceso
+admin.site.register(Prueba) # Table Prueba/ tabla prueba
+admin.site.register(Resultado) # Table Resultados / tabla resultado
