73f28c5ae20f1473164144f22222c34f825e1048
Ricardo <ricardo.vaca@udla.edu.ec>
2025-02-13 11:16:02 -0500

    Se crea el modelo Aeronave para registrar de manera más completa la información de Vuelos

---
+++ aeronave/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from aeronave.models import Aeronave
+# Register your models here.
+
+admin.site.register(Aeronave)
---
+++ aeronave/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Aeronave(models.Model):
+    id_aeronave = models.AutoField(primary_key = True)
+    TIPO_AERONAVE = {
+        "Cargo":"Cargo",
+        "PAX":"PAX"
+    }
+    tipo_de_aeronave =  models.CharField (max_length = 6, choices =TIPO_AERONAVE )
+    matricula_aeronave = models.CharField(max_length = 10)
+    nombre_aeronave = models.CharField(max_length = 30)
+    aerolinea = models.CharField(max_length = 30)
+    
+    def __str__(self):
+        return f"Aeronave: {self.tipo_de_aeronave} - Matricula: {self.matricula_aeronave} - Nombre: {self.nombre_aeronave} - Aerolinea: {self.aerolinea} "
+
+# Create your models here.
