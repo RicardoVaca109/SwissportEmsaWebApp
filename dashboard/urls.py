a0e7f9ef061cbef5a8ae2c28d80d7a914f1768bc
Ricardo <ricardo.vaca@udla.edu.ec>
2025-02-25 14:23:13 -0500

    Se agrega las Urls para poder ingresar al Dashboard de accesos

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from dashboard.views import navegacion_dashboard_general, navegacion_dashboard_rol_ojt
+from dashboard.views import navegacion_dashboard_general, navegacion_dashboard_rol_ojt, navegacion_dashboard_accesos
 
 urlpatterns = [
     # path('', include('website.urls.auth_urls')),
     # path('dashboard/', navegacion_dashboard_general, name='dashboard_general'),
-    path('dashboard/ojt/', navegacion_dashboard_rol_ojt, name = 'dashboard_rol_ojt' ),
+    path('dashboard_ojt/', navegacion_dashboard_rol_ojt, name = 'dashboard_rol_ojt' ),
+    path('dashboard_accesos/', navegacion_dashboard_accesos, name = 'dashboard_accesos' ),
     path('', navegacion_dashboard_general, name='dashboard_general'),
     path('aeropuertos/', include('aeropuertos.urls')),
     path('usuarios/',include('usuarios.urls')),
