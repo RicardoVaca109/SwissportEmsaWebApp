00aed4e2feafba7b97ace0302a0f5f62c5126db1
Ricardo <ricardo.vaca@udla.edu.ec>
2025-02-25 13:35:33 -0500

    Actualizacion en los URLs para buscar accesos que tengan los usuarios

--- accesos/urls.py
+++ accesos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accesos.views import navegacion_dashboard_accesos, agregar_add_accesos
+from accesos.views import navegacion_dashboard_accesos, agregar_add_accesos, look_buscar_usuario_accesos
 
 urlpatterns = [
     path('', navegacion_dashboard_accesos, name = 'dashboard_accesos'),
-    path('agregar/', agregar_add_accesos, name = 'add_accesos' )
+    path('buscar_usuario_accesos', look_buscar_usuario_accesos, name = 'buscar_usuario_accesos'),
+    path('agregar_usuario_accesos/', agregar_add_accesos, name = 'add_accesos' )
 ]
