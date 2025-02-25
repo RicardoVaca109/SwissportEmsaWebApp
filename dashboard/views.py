a269d150ca5b862300fc23dc05d72d71a811e7d1
Ricardo <ricardo.vaca@udla.edu.ec>
2025-02-25 14:22:07 -0500

    Cambio en el view de dashboard para navegar al dashboard que nos permite agregar accesos

--- dashboard/views.py
+++ dashboard/views.py
@@ -12,3 +12,6 @@ def navegacion_dashboard_rol_ojt(request):
     usuario_nombre = request.session.get('usuario_nombre', 'Usuario')
     return render(request, "dashboard_rol_ojt.html", {"usuario_nombre": usuario_nombre})
 
+def navegacion_dashboard_accesos(request):
+    usuario_nombre = request.session.get('usuario_nombre', 'Usuario')
+    return render(request, "dashboard_accesos.html", {"usuario_nombre": usuario_nombre})
