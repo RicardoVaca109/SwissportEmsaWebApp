2885da36e53cc7334c31274c1c9f681a9332d8ad
Ricardo <ricardo.vaca@udla.edu.ec>
2025-02-25 14:23:54 -0500

    Se agrega URL para editar usuario llamando por ID

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from usuarios.views import navegacion_dashboard_usuarios, agregar_add_usuarios
+from usuarios.views import navegacion_dashboard_usuarios, agregar_add_usuarios, editar_edit_usuarios
 
 urlpatterns = [
     
     path('', navegacion_dashboard_usuarios, name ='dashboard_usuarios' ),
-    path('agregar_usuario/', agregar_add_usuarios, name = 'add_usuarios')
+    path('agregar_usuario/', agregar_add_usuarios, name = 'add_usuarios'),
+    path('usuarios/editar/<int:id_usuario>/', editar_edit_usuarios, name = 'edit_usuarios')
 ]
