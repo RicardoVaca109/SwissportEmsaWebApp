d73669f830a2b32441dc605f7746ec5f0f8da220
Ricardo <ricardo.vaca@udla.edu.ec>
2025-02-27 08:12:37 -0500

    Se añade el URL para editar un vuelo

--- vuelos/urls.py
+++ vuelos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from vuelos.views import navegacion_dashboard_vuelos, agregar_add_vuelos
+from vuelos.views import navegacion_dashboard_vuelos, agregar_add_vuelos, editar_edit_vuelos
 
 urlpatterns = [
     # Principal Route/ Ruta principal
     path('', navegacion_dashboard_vuelos, name = 'dashboard_vuelos'),
-    path('agregar_vuelo/', agregar_add_vuelos, name = 'add_vuelos')
+    path('agregar_vuelo/', agregar_add_vuelos, name = 'add_vuelos'),
+    path('vuelos/editar/<int:id_vuelo>', editar_edit_vuelos, name = 'edit_vuelos')
 ]
