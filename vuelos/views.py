2661c876541a18a2e8564b17c7171f7570d7e65a
Ricardo <ricardo.vaca@udla.edu.ec>
2025-02-27 08:12:59 -0500

    Se crea el view para editar un vuelo

--- vuelos/views.py
+++ vuelos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from vuelos.models import Vuelo
 from aeropuertos.models import Aeropuerto
@@ -20,7 +20,7 @@ def agregar_add_vuelos(request):
         codigo_del_vuelo = request.POST.get('codigo_del_vuelo')
         origen_vuelo = request.POST.get('origen_vuelo')
         destino_vuelo = request.POST.get('destino_vuelo')
-        escala_intermedia = request.POST.get('escala_intermedia')
+        escala_intermedia = request.POST.get('escala_intermedia') or None
         aeronave = request.POST.get('aeronave')
         hora_llegada = request.POST.get ('hora_llegada')
         hora_salida = request.POST.get ('hora_salida')
@@ -38,4 +38,32 @@ def agregar_add_vuelos(request):
             )
         return redirect(reverse('dashboard_vuelos'))
     return render(request, 'add_vuelos_tmp.html', {'total_aeropuertos':total_aeropuertos, 'total_aeronaves':total_aeronaves})
+
+def editar_edit_vuelos(request, id_vuelo):
+    
+    vuelo = get_object_or_404(Vuelo, id_vuelo = id_vuelo)
+    
+    #Obtain all Airports / Obtener todo los aeropuertos
+    total_aeropuertos = Aeropuerto.objects.all()
+    # Obtain all Aeronaves / Obtener todas las aeronaves
+    total_aeronaves = Aeronave.objects.all()
+    
+    if request.method == 'POST':
+        vuelo.fecha_vuelo = request.POST.get('fecha_vuelo')
+        vuelo.codigo_del_vuelo = request.POST.get('codigo_del_vuelo')
+        vuelo.origen_vuelo = Aeropuerto.objects.get(pk = request.POST.get('origen_vuelo'))
+        vuelo.destino_vuelo = Aeropuerto.objects.get(pk = request.POST.get('destino_vuelo'))
+        escala_intermedia_id = request.POST.get('escala_intermedia') or None
+        vuelo.escala_intermedia = Aeropuerto.objects.get(pk = escala_intermedia_id) if escala_intermedia_id else None
+        vuelo.aeronave = Aeronave.objects.get(pk = request.POST.get('aeronave'))
+        vuelo.hora_salida = request.POST.get ('hora_salida')
+        vuelo.hora_llegada = request.POST.get('hora_llegada')
+        vuelo.save()
+        return redirect(reverse('dashboard_vuelos'))
+    return render(request, 'edit_vuelos_tmp.html',{
+        'vuelo' : vuelo,
+        'total_aeropuertos' : total_aeropuertos,
+        'total_aeronaves' : total_aeronaves,
+    })
+        
         
