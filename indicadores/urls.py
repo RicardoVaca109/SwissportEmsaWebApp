2effb78f7e606e84d102559ff403a216bbceb41d
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-05 12:37:11 -0500

    Se agregan las funciones del View como Rutas en Urls

--- indicadores/urls.py
+++ indicadores/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from indicadores.views import navegacion_dashboard_indicadores
+from indicadores.views import navegacion_dashboard_indicadores, look_buscar_usuario_indicadores, agregar_add_indicador
 
 urlpatterns = [
-    path('', navegacion_dashboard_indicadores, name = 'dashboard_indicadores')
+    path('', navegacion_dashboard_indicadores, name = 'dashboard_indicadores'),
+    path('buscar_usuario_agregar_indicadores/', look_buscar_usuario_indicadores, name = 'buscar_usuario_indicadores'),
+    path('agregar_indicador_usuario/<int:usuario_id>/', agregar_add_indicador, name = 'add_indicador')
 ]
