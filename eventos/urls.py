98db396cd0ec1e86f89c316b7089e4c0f365f635
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-05 12:36:54 -0500

    Se agregan las funciones del View como Rutas en Urls

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from eventos.views import navegacion_dashboard_eventos, agregar_add_eventos
+from eventos.views import navegacion_dashboard_eventos, agregar_add_eventos,look_buscar_usuario_eventos, look_buscar_vuelo_eventos
 
 urlpatterns = [
     path('', navegacion_dashboard_eventos, name = 'dashboard_eventos'),
-    path('agregar_eventos/', agregar_add_eventos, name = 'add_eventos' )
+    path('agregar_eventos/', agregar_add_eventos, name = 'add_eventos' ),
+    path('buscar_usuario_asignar_eventos/', look_buscar_usuario_eventos, name = 'buscar_usuario_eventos'),
+    path('buscar_vuelo_asignar_eventos/', look_buscar_vuelo_eventos, name = 'buscar_vuelo_eventos')
 ]
