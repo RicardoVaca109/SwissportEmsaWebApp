5a8fdc26a294e4666a8c0763f97fad7997a2ea0a
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-06 12:24:18 -0500

    Se modifica las Ursl para agregar la de la búsqueda dinámica de vuelos

--- atrasos/urls.py
+++ atrasos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from atrasos.views import navegacion_dashboard_atrasos, agregar_add_atrasos
+from atrasos.views import navegacion_dashboard_atrasos, agregar_add_atrasos, look_buscar_vuelo_atrasos
 
 urlpatterns = [
     path('', navegacion_dashboard_atrasos, name = 'dashboard_atrasos'),
-    path('agregar/', agregar_add_atrasos, name = 'add_atrasos' )
+    path('agregar_atraso/', agregar_add_atrasos, name = 'add_atrasos' ),
+    path('buscar_vuelo_agregar_atras/', look_buscar_vuelo_atrasos, name = 'buscar_vuelo_atrasos')
 ]
