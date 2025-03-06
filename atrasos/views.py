eabd42962a33f5ce48926378956d57ad3900477f
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-06 12:23:38 -0500

    Se añade búsqueda dinámica de Vuelos como funcion para registrar un atraso

--- atrasos/views.py
+++ atrasos/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import JsonResponse
 from atrasos.models import Atraso
 from vuelos.models import Vuelo
 
 def navegacion_dashboard_atrasos(request):
     total_atrasos = Atraso.objects.all()
     return render(request, 'dashboard_atrasos.html', {'total_atrasos':total_atrasos})
+
+def look_buscar_vuelo_atrasos(request):
+    query_busqueda_vuelo = request.GET.get('q', '').strip()
+    
+    vuelos = Vuelo.objects.all()
+    
+    if query_busqueda_vuelo:
+        vuelos = vuelos.filter(codigo_del_vuelo__icontains=query_busqueda_vuelo)
+    
+    # Convertir los resultados a un formato que pueda ser enviado como JSON
+    vuelos_json = [{
+        'id_vuelo': vuelo.id_vuelo,
+        'codigo_del_vuelo': vuelo.codigo_del_vuelo,
+        'origen_vuelo': vuelo.origen_vuelo.estacion_aeropuerto,
+        'destino_vuelo': vuelo.destino_vuelo.estacion_aeropuerto,
+        'fecha_vuelo': vuelo.fecha_vuelo.strftime('%Y-%m-%d')  # Formatear la fecha
+    } for vuelo in vuelos]
+    
+    return JsonResponse({'vuelos': vuelos_json})
 
 def agregar_add_atrasos(request):
     # Vuelos totales 
