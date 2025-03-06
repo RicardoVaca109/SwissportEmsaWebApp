811a94508ef7de9d6d7eb44b2399cef6d9957a9c
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-06 12:28:19 -0500

    Se elemina la redundancia para dirigirse al dahsboard de Accesos

--- accesos/urls.py
+++ accesos/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from accesos.views import navegacion_dashboard_accesos, agregar_add_accesos, look_buscar_usuario_accesos
+from accesos.views import agregar_add_accesos, look_buscar_usuario_accesos
 
 urlpatterns = [
-    path('', navegacion_dashboard_accesos, name = 'dashboard_accesos'),
     path('buscar_usuario_accesos', look_buscar_usuario_accesos, name = 'buscar_usuario_accesos'),
     path('agregar_usuario_accesos/', agregar_add_accesos, name = 'add_accesos_usuarios' )
 ]
