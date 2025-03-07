27ffc6b218a02223137f95a0a711a3e059d72f38
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-07 12:10:08 -0500

    Se crea la función para poder agregar un registro de calificación de una Guía

--- guias/views.py
+++ guias/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.urls import reverse
 from guias.models import Guia
+from guias.models import Calificacion_Guia
+from usuarios.models import Usuario
 
 def navegacion_dashboard_guias(request):
-    total_guias = Guia.objects.all()
-    return render(request, 'dashboard_guias.html', {'total_guias':total_guias} )
+    # total_guias = Guia.objects.all()
+    return render(request, 'dashboard_guias.html' )
+
+def agregar_add_calificacion_guia(request):
+    if 'usuario_id' not in request.session:
+        messages.error(request, "Debes Iniciar Sesión para Evaluar")
+        return redirect('login_view')
+
+    usuario_calificador = get_object_or_404(Usuario, id_usuario=request.session['usuario_id'])
+    guias_totales = Guia.objects.all()
+
+    if request.method == 'POST':
+        guia_id = request.POST.get('id_guia')
+        calificacion = request.POST.get('calificacion')
+        observacion = request.POST.get('observacion')
+        fecha_calificacion = request.POST.get('fecha_calificacion')
+
+        # Validar que los datos no estén vacíos
+        if not guia_id or not calificacion or not observacion or not fecha_calificacion:
+            messages.error(request, "Todos los campos son obligatorios.")
+            return redirect('dashboard_guias')
+
+        try:
+            guia = get_object_or_404(Guia, pk=guia_id)
+            calificacion = int(calificacion)
+            Calificacion_Guia.objects.create(
+                guia=guia,
+                usuario_calificador=usuario_calificador,
+                calificacion=calificacion,
+                observacion=observacion,
+                fecha_calificacion=fecha_calificacion
+            )
+            return redirect('dashboard_guias')
+
+        except ValueError:
+            messages.error(request, "Error en los datos ingresados.")
+            return redirect('dashboard_guias')
+
+    return render(request, 'add_guias_tmp.html', {'guias': guias_totales})
+        
