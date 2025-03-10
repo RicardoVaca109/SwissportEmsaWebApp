897c3807e740035164c91a2ee062eeaf0f258913
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-10 13:49:23 -0500

    Se agrega Url para ver las guias calificadas por cada usuario en sesión

--- guias/urls.py
+++ guias/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from guias.views import navegacion_dashboard_guias
+from guias.views import navegacion_dashboard_guias, agregar_add_calificacion_guia
 
 urlpatterns = [
-    path('', navegacion_dashboard_guias, name = 'dashboard_guias' )
+    path('', navegacion_dashboard_guias, name = "dashboard_guias" ),
+    path('agregar_califcacion_guia/', agregar_add_calificacion_guia, name = 'add_calificacion_guia')
 ]
