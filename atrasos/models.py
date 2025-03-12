a946231c6f9c59c3ba94b314ca1e85eb0b486e32
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-12 12:12:18 -0500

    Se aumenta el tamaño del atributo motivo_atraso

--- atrasos/models.py
+++ atrasos/models.py
@@ -7,7 +7,7 @@ class Atraso(models.Model): # Table Atrasos/ Tabla Atrasos
     fecha_atraso = models.DateField(auto_now = False, auto_now_add = False)
     codigo_atraso = models.CharField(max_length = 10)
     minutos = models.IntegerField(default = 0)
-    motivo_atraso = models.CharField(max_length = 150)
+    motivo_atraso = models.CharField(max_length = 250)
     
     def  __str__(self):
         return f"Vuelo: {self.vuelo.codigo_del_vuelo} | Matrícula: {self.vuelo.aeronave.matricula_aeronave} | Destino: {self.vuelo.destino_vuelo} |Fecha: {self.fecha_atraso} | Codigo: {self.codigo_atraso} | Motivo: {self.motivo_atraso}"
