d3d90c4b6b881534cdcdae8c176ac8e390f44e56
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-13 08:18:26 -0500

    Se añade la Url para añadir un Evento

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from eventos.views import navegacion_dashboard_eventos, agregar_add_eventos,look_buscar_usuario_eventos, look_buscar_vuelo_eventos
+from eventos.views import navegacion_dashboard_eventos, agregar_add_eventos,look_buscar_usuario_eventos, look_buscar_vuelo_eventos, editar_edit_evento
 
 urlpatterns = [
     path('', navegacion_dashboard_eventos, name = 'dashboard_eventos'),
     path('agregar_eventos/', agregar_add_eventos, name = 'add_eventos' ),
     path('buscar_usuario_asignar_eventos/', look_buscar_usuario_eventos, name = 'buscar_usuario_eventos'),
-    path('buscar_vuelo_asignar_eventos/', look_buscar_vuelo_eventos, name = 'buscar_vuelo_eventos')
+    path('buscar_vuelo_asignar_eventos/', look_buscar_vuelo_eventos, name = 'buscar_vuelo_eventos'),
+    path('editar/<int:id_evento>/', editar_edit_evento, name = 'edit_evento')
 ]
