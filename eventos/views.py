ec9504c41ceab8bd8e69eebc45e46579570d3967
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-13 08:18:09 -0500

    En las búsqueda de vuelos se aumenta la hora y salida en formato JSON al ser atributos de tiempo. Se agrega también la función para editar un Evento

--- eventos/views.py
+++ eventos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from django.urls import reverse
 from django.http import JsonResponse
@@ -47,7 +47,9 @@ def look_buscar_vuelo_eventos(request):
         'codigo_del_vuelo': vuelo.codigo_del_vuelo,
         'origen_vuelo': vuelo.origen_vuelo.estacion_aeropuerto,
         'destino_vuelo': vuelo.destino_vuelo.estacion_aeropuerto,
-        'fecha_vuelo': vuelo.fecha_vuelo.strftime('%Y-%m-%d')  # Formatear la fecha
+        'fecha_vuelo': vuelo.fecha_vuelo.strftime('%Y-%m-%d'),# Formatear la fecha
+        'hora_salida': vuelo.hora_salida.strftime('%H:%M:%S'),
+        'hora_llegada': vuelo.hora_llegada.strftime('%H:%M:%S'),
     } for vuelo in vuelos]
     
     return JsonResponse({'vuelos': vuelos_json})
@@ -83,3 +85,33 @@ def agregar_add_eventos(request):
             )
             return redirect(reverse('dashboard_eventos'))
     return render(request, "add_eventos_tmp.html", {"total_vuelos": total_vuelos, "total_usuarios": total_usuarios})
+
+
+def editar_edit_evento(request, id_evento):
+    evento = get_object_or_404(Evento, id_evento = id_evento)
+    
+    # Vuelos totales 
+    total_vuelos = Vuelo.objects.all()
+    # Usuarios totales
+    total_usuarios = Usuario.objects.all()
+    
+    if request.method == 'POST':
+        evento.vuelo = Vuelo.objects.get(pk = request.POST.get('id_vuelo'))
+        evento.usuario_involucrado = Usuario.objects.get(pk = request.POST.get('usuario_involucrado'))
+        evento.fecha_evento = request.POST.get('fecha_evento')
+        evento.evento_lir = request.POST.get('evento_lir') 
+        evento.evento_pax = request.POST.get('evento_pax')
+        evento.motivo = request.POST.get('motivo') 
+        evento.tipo_evento = request.POST.get('tipo_evento')
+        evento.plan_de_accion = request.POST.get('plan_de_accion')
+        evento.save()
+        return redirect(reverse('dashboard_eventos'))
+    return render(request, 'edit_eventos_tmp.html', {
+        'evento':evento,
+        'total_vuelos':total_vuelos,
+        'total_usuarios':total_usuarios,       
+    })
+        
+        
+    
+    
