5afd4c46bf14bdad04b7fce4651ce571aca591d0
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-14 08:59:56 -0500

    Cambios mínimos

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from dashboard.views import navegacion_dashboard_general, navegacion_dashboard_rol_ojt, navegacion_dashboard_rol_kam_supervisor
 
 urlpatterns = [
-    # path('', include('website.urls.auth_urls')),
     # path('dashboard/', navegacion_dashboard_general, name='dashboard_general'),
     path('dashboard_ojt/', navegacion_dashboard_rol_ojt, name = 'dashboard_rol_ojt' ),
     path('dashboard_kams_super/', navegacion_dashboard_rol_kam_supervisor, name = 'dashboard_rol_kam_supervisor' ),
