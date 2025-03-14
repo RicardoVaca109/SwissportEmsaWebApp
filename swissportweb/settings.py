23597c54270f5248aa8dead9d1b36b2e17d644dd
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-14 12:53:09 -0500

    Se agrega las funcionalidades del MiddleWare del archivo middleware.py de la aplicación auth_custom en el settings.py de la aplicacion principal

--- swissportweb/settings.py
+++ swissportweb/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'auth_custom.middleware.AuthMiddleware',
 ]
 
 ROOT_URLCONF = 'swissportweb.urls'
