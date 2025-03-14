a8871ed01af865d0d9f3cba0063afdd33715ef2e
Ricardo <ricardo.vaca@udla.edu.ec>
2025-03-14 08:59:36 -0500

    Cambios mínimos

--- auth_custom/views.py
+++ auth_custom/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import check_password
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
 from django.contrib import messages
 from usuarios.models import Usuario
 
